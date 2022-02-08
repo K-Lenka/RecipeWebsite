b8c2b511ce851244775871bea82d6cbe6da6eeae
Elena <alena1027@gmail.com>
2022-02-08 19:52:32 -0400

    feat: create variables, add event listner on btn

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
 // fetch('https://api.spoonacular.com/recipes/random?apiKey=0a0760ff66be4930825f532ce26fa3eb').then(resp => resp.json())
-//         .then(recipies => document.getElementById('data').innerText = JSON.stringify(recipies, null, 2));
+//         .then(recipies => 
+
+const searchBtn = document.getElementById('search-btn');
+const recipeList = document.getElementById('recipe--list');
+
+// Event listeners
+searchBtn.addEventListener('click', getMealList);
+recipeList.addEventListener('click', getMealRecipe);
