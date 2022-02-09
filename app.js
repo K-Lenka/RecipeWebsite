7c813178dcd15138c8cf7bb4ac5f72da87f7d8c0
Elena <alena1027@gmail.com>
2022-02-08 20:35:35 -0400

    feat: create getMealList function

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
-// fetch('https://api.spoonacular.com/recipes/random?apiKey=0a0760ff66be4930825f532ce26fa3eb').then(resp => resp.json())
-//         .then(recipies => 
-
 const searchBtn = document.getElementById('search-btn');
 const recipeList = document.getElementById('recipe--list');
 
 // Event listeners
 searchBtn.addEventListener('click', getMealList);
-recipeList.addEventListener('click', getMealRecipe);
+
+// get food list 
+function getMealList(){
+    let searchInput = document.getElementById('search-input')
+
+    searchInput.value.trim()
+
+    fetch(`https://api.spoonacular.com/recipes/random?apiKey=0a0760ff66be4930825f532ce26fa3eb`)
+    .then(response => response.json())
+    .then(data => console.log(data))
+    
+}
