b659a4ee9c66f03089ecb74bde80760e64a201ad
Elena <alena1027@gmail.com>
2022-02-08 22:31:25 -0400

    feat: update getMealList function,add event listner on Enter btn

--- app.js
+++ app.js
@@ -1,17 +1,40 @@
 const searchBtn = document.getElementById('search-btn');
-const recipeList = document.getElementById('recipe--list');
+const recipeList = document.getElementById('recipe-list');
+const searchInput = document.getElementById('search-input');
 
-// Event listeners
+// Event listener
 searchBtn.addEventListener('click', getMealList);
 
-// get food list 
-function getMealList(){
-    let searchInput = document.getElementById('search-input')
+searchInput.addEventListener('keyup', function(e){
+    if(e.key === 'Enter'){
+       getMealList();
+    }
+});
 
-    searchInput.value.trim()
+// get food list
+function getMealList() {
+  let input = searchInput.value.trim();
 
-    fetch(`https://api.spoonacular.com/recipes/random?apiKey=0a0760ff66be4930825f532ce26fa3eb`)
-    .then(response => response.json())
-    .then(data => console.log(data))
-    
+  fetch(
+    `https://api.spoonacular.com/recipes/complexSearch?apiKey=0a0760ff66be4930825f532ce26fa3eb&query=${input}&number=5&offset=0`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data)
+      let html = "";
+      if (data.results) {
+        data.results.forEach((result) => {
+          html += ` <div class = "meal-item" data-id = "${result.id}">
+                <div class = "meal-img">
+                    <img src = "${result.image}" alt="food">
+                </div>
+                <div class = "meal-name">
+                    <h3>${result.title}</h3>
+                    <a href = "#" class = "recipe-btn">Get Recipe</a>
+                </div>
+            </div>`;
+        });
+      }
+      recipeList.innerHTML = html;
+    });
 }
