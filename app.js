583d71f0463023fe0b34b4dd3115aacb89f343c3
Elena <alena1027@gmail.com>
2022-02-09 20:05:58 -0400

    feat: style recipe result outputs,add media query, delete img

--- app.js
+++ app.js
@@ -38,3 +38,15 @@ function getMealList() {
       recipeList.innerHTML = html;
     });
 }
+
+// get recipe
+
+function getMealRecipe(e){
+  e.preventDefault();
+  if(e.target.classList.contains('recipe-btn')){
+      let mealItem = e.target.parentElement.parentElement;
+      fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.dataset.id}`)
+      .then(response => response.json())
+      .then(data => mealRecipeModal(data.meals));
+  }
+}
